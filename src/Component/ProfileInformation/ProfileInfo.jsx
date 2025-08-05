9d9e45bec3cd4addd2337756432f37988098c784
Yadhukrishna123 <yadhumv365@gmail.com>
2025-08-05 11:52:50 -0700

    add progileInfo.css

--- src/Component/ProfileInformation/ProfileInfo.jsx
+++ src/Component/ProfileInformation/ProfileInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
-import "./Profileinfo.css"
+import "./ProfileInfo.css"
 import { useSelector } from 'react-redux'
 
 const ProfileInfo = () => {
